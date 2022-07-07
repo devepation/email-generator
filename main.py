ecf70a3f8874d38f60961fe8b18e9cd97b0109bf
Tyler <tylerfromgw@gmail.com>
2022-07-07 11:43:26 -0500

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import random
+import string
+import json
+from time import sleep as wait
+
+namesl = json.loads(open('names.json').read())
+
+names = []
+for name in namesl:
+    name = name.lower()
+    name_e = ''.join(random.choice(string.digits)+random.choice(string.digits)+random.choice(string.digits)+random.choice(string.digits))
+    name = name + name_e
+    names.append(name)
+#print(names)
+def doit():
+    return random.choice(names) + "@gmail.com"
+    
+while True:
+    print(doit())
+    wait(0.002)
